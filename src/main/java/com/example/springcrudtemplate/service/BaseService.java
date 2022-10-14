6db99f9a2b3c7f89e72a94aa5833a7aec188d2a3
Jewoo <jeus0630@gmail.com>
2022-10-15 01:29:42 +0900

    Fix `BaseRepository` to `BaseService`

--- src/main/java/com/example/springcrudtemplate/service/BaseService.java
+++ src/main/java/com/example/springcrudtemplate/service/BaseService.java
@@ -1,9 +1,10 @@
-package com.example.springcrudtemplate.repository;
+package com.example.springcrudtemplate.service;
 
+import com.example.springcrudtemplate.ifs.CrudInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public abstract class BaseRepository<Entity> {
+public abstract class BaseService<Req, Res, Entity> implements CrudInterface<Req, Res> {
 
     @Autowired(required = false)
     protected JpaRepository<Entity, Long> baseRepository;
